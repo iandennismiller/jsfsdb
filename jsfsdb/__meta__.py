91731cd60c97b99cde13e31422faab5e8d63925e
Ian Dennis Miller <ian@iandennismiller.com>
2021-09-24 07:18:48 -0400

    relax requirement for jq version

--- jsfsdb/__meta__.py
+++ jsfsdb/__meta__.py
@@ -2,7 +2,7 @@
 # jsfsdb (c) Ian Dennis Miller
 
 __project__ = 'jsfsdb'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Ian Dennis Miller'
 __email__ = 'ian@iandennismiller.com'
 __url__ = 'http://jsfsdb.readthedocs.io'
